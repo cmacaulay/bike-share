f11c7e606458be8593d66ea22684bb26f967a79c
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-01-27 00:55:34 -0700

    completes create and add functionality for station.

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -5,9 +5,19 @@ class BikeShareApp < Sinatra::Base
     erb :'stations/index'
   end
 
+  get '/stations/new' do
+    @station = Station.new
+    erb :'stations/new'
+  end
+
   get '/stations/:id' do
     @station = Station.find(params[:id])
     erb :'stations/show'
   end
 
+  post '/stations' do
+    @station = Station.create(params[:station])
+    redirect "/stations/#{@station.id}"
+  end
+  
 end
