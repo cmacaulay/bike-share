f6019cb914edc71bed07823057f721fce46a1493
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-01-27 00:36:50 -0700

    adds show view and route for stations

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,7 +1,13 @@
 class BikeShareApp < Sinatra::Base
+
   get '/stations' do
     @stations = Station.all
     erb :'stations/index'
   end
-  
+
+  get '/stations/:id' do
+    @station = Station.find(params[:id])
+    erb :'stations/show'
+  end
+
 end
