80a7551523dc9acf51bc41cae71cae705fdc58a2
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-01-29 15:58:30 -0700

    updates station table to include city_id in the schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129010343) do
+ActiveRecord::Schema.define(version: 20170129011207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,10 +24,10 @@ ActiveRecord::Schema.define(version: 20170129010343) do
   create_table "stations", force: :cascade do |t|
     t.string   "name"
     t.integer  "dock_count"
-    t.string   "city"
     t.date     "installation_date"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.integer  "city_id"
   end
 
 end
