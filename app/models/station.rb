db813b90623114d67db2165ba7aae3c81d79db2c
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-01-29 15:21:35 -0700

    adds dashboard methods total count, average bike, most bikes, stations where most bikes are available and most recently installed station. tests for all dashboard methods except the latter

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,32 @@
 class Station < ActiveRecord::Base
-  validates :name, :dock_count, :city, :installation_date, presence: true
+  validates :name,
+            :dock_count,
+            :city,
+            :installation_date,
+            presence: true
+
+  def self.total_stations
+    count(:all)
+    #all.count (plan b)
+  end
+
+  def self.average_bikes_per_station
+    average(:dock_count).to_f.round(2)
+  end
+
+  def self.max_bikes
+    #most bikes available at a station
+    maximum(:dock_count)
+  end
+
+  def self.stations_with_most_bikes
+    #stations where most bikes are available
+    return [] if max_bikes.nil?
+    where("dock_count = #{max_bikes}")
+  end
+
+  def self.newest_station
+    # station most recently installed
+    order(:installation_date).last
+  end
 end
