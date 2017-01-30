ef5c91d26ef217a62b4e075907a0bf7b31d99b6e
Cody Borders <codyborders@gmail.com>
2017-01-30 15:50:23 -0700

    Adds working tests for oldest station, newest station, and fewest bikes

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../spec_helper'
+require 'pry'
 
 RSpec.describe Station do
   describe 'validations' do
@@ -107,37 +108,39 @@ RSpec.describe Station do
 
     describe '.fewest_bikes' do
     it "returns fewest_bikes" do
-      Station.create(name: "Station1", dock_count: 7, city: "San Francisco", installation_date: "2013-03-10")
-      Station.create(name: "Station2", dock_count: 5, city: "San Francisco", installation_date: "2013-03-10")
-      Station.create(name: "Station3", dock_count: 10, city: "San Francisco", installation_date: "2013-03-10")
-      result = Station.fewest_bikes
+      san_francisco = City.create(name: "San Francisco")
+      oakland       = City.create(name: "Oakland")
+      berkeley      = City.create(name: "Berkeley")
 
-      expect(result.first.name).to eq("Station2")
-      expect(result.last.name).to eq("Station3")
+      san_francisco.stations.create(name: "Station1", dock_count: 5, installation_date: "2013-03-10")
+      oakland.stations.create(name: "Station2", dock_count: 7, installation_date: "2013-05-02")
+      berkeley.stations.create(name: "Station3", dock_count: 10, installation_date: "2013-06-12")
+      result = Station.fewest_bikes
+      expect(result).to eq(5)
     end
   end
 
   describe '.oldest_station' do
     it "returns oldest station" do
-      Station.create(name: "Station1", dock_count: 5, city: "San Francisco", installation_date: "2013-03-10")
-      Station.create(name: "Station2", dock_count: 5, city: "San Francisco", installation_date: "2013-05-02")
-      Station.create(name: "Station3", dock_count: 5, city: "San Francisco", installation_date: "2013-06-12")
+      Station.create(name: "Station1", dock_count: 5, city: City.create(name:"San Francisco"), installation_date: "2013-03-10")
+      Station.create(name: "Station2", dock_count: 5, city: City.create(name:"San Francisco"), installation_date: "2013-05-02")
+      Station.create(name: "Station3", dock_count: 5, city: City.create(name:"San Francisco"), installation_date: "2013-06-12")
       result = Station.oldest_station
 
-      expect(result.first.name).to eq("Station1")
-      expect(result.last.name).to eq("Station3")
+      expect(result.name).to eq("Station1")
+
     end
   end
 
   describe '.newest_station' do
     it "returns newest station" do
-      Station.create(name: "Station1", dock_count: 5, city: "San Francisco", installation_date: "2013-03-10")
-      Station.create(name: "Station2", dock_count: 5, city: "San Francisco", installation_date: "2013-05-02")
-      Station.create(name: "Station3", dock_count: 5, city: "San Francisco", installation_date: "2013-06-12")
+      Station.create(name: "Station1", dock_count: 5, city: City.create(name:"San Francisco"), installation_date: "2013-03-10")
+      Station.create(name: "Station2", dock_count: 5, city: City.create(name:"San Francisco"), installation_date: "2013-05-02")
+      Station.create(name: "Station3", dock_count: 5, city: City.create(name:"San Francisco"), installation_date: "2013-06-12")
       result = Station.newest_station
 
-      expect(result.first.name).to eq("Station3")
-      expect(result.last.name).to eq("Station1")
+      expect(result.name).to eq("Station3")
+
     end
   end
 
