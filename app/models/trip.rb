a2f79019ede0ea775d7da4e632337769082d86cb
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-01-30 19:01:29 -0700

    adds shortest ride, longest ride, average ride, most ridden bike and least ridden bike methods to trip model and tests for all of them

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -12,4 +12,30 @@ class Trip < ActiveRecord::Base
   belongs_to :subscription
   belongs_to :start_station, class_name: "Station", foreign_key: "start_station_id"
   belongs_to :end_station, class_name: "Station", foreign_key: "end_station_id"
-end 
+
+  def self.shortest_ride
+    minimum(:duration)
+  end
+
+  def self.longest_ride
+    maximum(:duration)
+  end
+
+  def self.average_ride_duration
+    average(:duration)
+  end
+
+  def self.most_ridden_bike
+    #with total numbers of rides for that bike most
+    group(:bike_id).count("id").max_by do |bike, count|
+      count
+    end
+    #returns array of [bike_ids, number of rides]
+  end
+
+  def self.least_ridden_bike
+    group(:bike_id).count("id").min_by do |bike, count|
+      count
+    end
+  end
+end
